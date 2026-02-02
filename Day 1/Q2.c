86ee2c65367a1e632c895cf7b706144130ad438b
Ayush Jha <ayushjhayt@gmail.com>
2026-02-02 12:19:29 +0530

    completed

--- Day 1/Q2.c
+++ Day 1/Q2.c
@@ -1,18 +1,17 @@
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. 
 
-int main() {
-    int nums[] = {2, 7, 11, 15};
-    int target = 9;
-    int n = 4;
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    static int result[2];
+    *returnSize = 2;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
             if (nums[i] + nums[j] == target) {
-                printf("[%d, %d]", i, j);
-                return 0;
+                result[0] = i;
+                result[1] = j;
+                return result;
             }
         }
     }
-
-    return 0;
+    return result;
 }
