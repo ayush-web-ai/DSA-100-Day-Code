484c684102113fdbf52a9fe8418f683408d9b296
Ayush Jha <ayushjhayt@gmail.com>
2026-02-10 21:41:07 +0530

    Create q19.c

---
+++ Day 10-19/q19.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+    int left, right, flag = 1;
+
+    // read string
+    scanf("%s", s);
+
+    left = 0;
+    right = strlen(s) - 1;
+
+    // two-pointer comparison
+    while (left < right) {
+        if (s[left] != s[right]) {
+            flag = 0;
+            break;
+        }
+        left++;
+        right--;
+    }
+
+    if (flag)
+        printf("YES");
+    else
+        printf("NO");
+
+    return 0;
+}
