00fe39419da427019338d3042b4f665c8f7e3af6
Ayush Jha <ayushjhayt@gmail.com>
2026-03-06 08:59:24 +0530

    completed

---
+++ Day 20-29/q48.c
@@ -0,0 +1,21 @@
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    // Create dummy node
+    struct ListNode dummy;
+    dummy.next = head;
+
+    struct ListNode* prev = &dummy;
+    struct ListNode* curr = head;
+
+    while (curr != NULL) {
+        if (curr->val == val) {
+            prev->next = curr->next;  // remove node
+            free(curr);               // free memory
+            curr = prev->next;        // move forward
+        } else {
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+
+    return dummy.next;
+}
